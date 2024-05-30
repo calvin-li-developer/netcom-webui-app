652e99886401eaa4fad6d7e475a180f6362fb2ec
Calvin Li <calvin.li.developer@gmail.com>
2024-05-30 10:37:41 -0400

    fix python app

--- python-app-secret/myapp.py
+++ python-app-secret/myapp.py
@@ -1,9 +1,10 @@
 import time
 
-while True:
+for i in range(3):
+    print(f"Running attempt {i}\n")
     print("Hello all , welcome to python..!!")
     time.sleep(3)
-    print("Welcome to NetCom BMO.. secrets are cool here")
+    print("Welcome to NetCom Learning Course..")
     time.sleep(2)
     print("Welcome to Containers ..!!")
     print("______________________")
