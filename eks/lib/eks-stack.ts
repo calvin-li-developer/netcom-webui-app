373759fb281ac9fd3153c7584a7d27ae5971b1a2
Calvin Li <65045619+calvin-li-developer@users.noreply.github.com>
2024-06-01 01:58:33 +0530

    deploy eks

--- eks/lib/eks-stack.ts
+++ eks/lib/eks-stack.ts
@@ -6,7 +6,7 @@ import * as iam from 'aws-cdk-lib/aws-iam'
 import * as eks from 'aws-cdk-lib/aws-eks'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class EcsStack extends cdk.Stack {
+export class EksStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -14,47 +14,23 @@ export class EcsStack extends cdk.Stack {
     const vpc = ec2.Vpc.fromLookup(this,'existing-vpc', {
       vpcId: 'vpc-063afa0c24ec80cb8'
     });
-    // defining ECS cluster info 
-    const cluster = new ecs.Cluster(this,'ecs-cluster',{
-      clusterName: 'calvinli-ecs-by-cdk',
-      vpc: vpc,
-      enableFargateCapacityProviders: true ,
-      containerInsights: true // enable cloudwatch monitoring 
-    });
-
-    // task Definition of farget launch type 
-    const taskDef = new ecs.FargateTaskDefinition(this,'task-def1',{
-      cpu:  256,
-      memoryLimitMiB: 512
-    });
-
-    const IMAGE_TAG = process.env.IMAGE_TAG as string;
-
-    if (!IMAGE_TAG) {
-      throw new Error("IMAGE TAG MISSING");
-    }
-    // adding container info 
-    const container = taskDef.addContainer('container-def1',{
-      image: ecs.ContainerImage.fromRegistry(`calvinlideveloper/netcomwebuiapp:${ IMAGE_TAG }`),
-      memoryLimitMiB: 256,
-    });
 
     // creating security group 
-    const secgroup = new ec2.SecurityGroup(this,'security-group1',{
+    const secgroup = new ec2.SecurityGroup(this,'security-group2',{
       vpc: vpc,
-      description: 'allow ingress rules for 80 port'
+      description: 'allow ingress rules for 8080 port'
     });
     secgroup.addIngressRule(ec2.Peer.anyIpv4(),ec2.Port.tcp(8080),'allow http traffic');
 
-    // adding Fargate Service
-    const service = new ecs.FargateService(this,'fargate-service1',{
-      cluster: cluster,
-      taskDefinition: taskDef,
-      serviceName: 'calvin-svc-by-cdk',
-      desiredCount: 1,
-      assignPublicIp: true,
-      securityGroups: [secgroup]   // attaching security group 
-    });
+    // // adding Fargate Service
+    // const service = new ecs.FargateService(this,'fargate-service1',{
+    //   cluster: cluster,
+    //   taskDefinition: taskDef,
+    //   serviceName: 'calvin-svc-by-cdk',
+    //   desiredCount: 1,
+    //   assignPublicIp: true,
+    //   securityGroups: [secgroup]   // attaching security group 
+    // });
 
     // creating IAM role for EKS cluster -- control plane 
     const iamRole = new iam.Role(this,'iam-role1',{
